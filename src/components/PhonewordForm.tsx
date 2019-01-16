85bfb95a13cfcda8ff971091ade0bec776482968
Albert Alises <albert.alises@gmail.com>
2019-01-16 16:46:36 +0100

    Add styling to the form

--- src/components/PhonewordForm.tsx
+++ src/components/PhonewordForm.tsx
@@ -2,19 +2,22 @@ import React from "react";
 import PhoneWordModel from "../models/phoneword-model";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 
+import Button from "@kiwicom/orbit-components/lib/Button";
+import InputField from "@kiwicom/orbit-components/lib/InputField";
+import Alert from "@kiwicom/orbit-components/lib/Alert";
+
 export default () => { 
     return (
         <Formik validateOnChange={false} onSubmit={(values, obj) => obj.setSubmitting(false)} initialValues={PhoneWordModel.initialValues} validationSchema={PhoneWordModel.ValidationSchema} >
             {props => {
-            const { isSubmitting, status, errors } = props;
+            const { isSubmitting, values, errors, handleChange, handleBlur } = props;
                 return (
                     <Form>
                         <div className="field">
-                            <label>Enter Phone Number</label>
-                            <Field name="number" placeholder="Phone Number" type="text" />
-                            <p><ErrorMessage name="number"/></p>
+                            <InputField label="Phone Number" name="number" value={values.number} onBlur={handleBlur} onChange={handleChange} placeholder="Phone Number" type="text" />
+                            {errors.number ? <Alert type="warning" title={null} icon><ErrorMessage name="number"/></Alert> : null}
                         </div>
-                        <button type="submit" disabled={isSubmitting}> Check Phone Word </button>
+                        <Button type="submit" disabled={isSubmitting}> Compute Phone Words </Button>
                     </Form>
                 );
             }}
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import styled from "styled-components";
 import PhonewordForm from "./components/PhonewordForm";
+
 import Heading from "@kiwicom/orbit-components/lib/Heading";
 
 const MainView = styled.div`
@@ -11,7 +12,7 @@ const MainView = styled.div`
 export default () => {
   return (
     <MainView>
-      <Heading type="display" element="h1">Phonewords Generator</Heading>
+      <Heading spaceAfter="large" type="display" element="h1">Phonewords Generator</Heading>
       <PhonewordForm />
     </MainView>
   )
