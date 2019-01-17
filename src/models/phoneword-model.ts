4b65fec86a17611326d3fe27f7872dbea6a8fd05
Albert Alises <albert.alises@gmail.com>
2019-01-17 12:54:39 +0100

    Adjust FE validation to not include 0 or 1

--- src/models/phoneword-model.ts
+++ src/models/phoneword-model.ts
@@ -8,12 +8,12 @@ export default class PhoneWordModel {
 
     static messages = {
         success: 'This is a valid phone number',
-        info: 'The phone number has to contain only digits e.g 690394837'
+        info: 'The phone number has to contain only digits from 2 to 9, e.g 69394837 (0 and 1 do not contain phone word information)'
     }
     
     static ValidationSchema = Yup.object().shape({
         number: Yup.string()
           .required('Phone number cannot be empty')
-          .matches(/^[0-9 ]+$/,{ message: 'Only integer numbers are allowed (0-9)', excludeEmptyString: true })
+          .matches(/^[2-9 ]+$/,{ message: 'Invalid number: Only integers from 2 to 9 are allowed', excludeEmptyString: true })
     });
 }
