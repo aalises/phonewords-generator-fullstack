ced797e9ac1414ac14b85ce568d6dcaa324a3090
Albert Alises <albert.alises@gmail.com>
2019-01-17 12:45:37 +0100

    rename to phonewords

--- backend/api/models/phonewords.py
+++ backend/api/models/phonewords.py
@@ -4,14 +4,14 @@ from functions.phoneword_algorithm import phonewords_from_number
 
 class PhoneWordModel():
 
-    def __init__(self, success, data, error):
+    def __init__(self, success, phonewords, error):
         self.success = success
-        self.data = data
+        self.phonewords = phonewords
         self.error = error
 
     def json(self):
         phoneword = [('success', self.success),
-                     ('data', self.data),
+                     ('phonewords', self.phonewords),
                      ('error', self.error)]
 
         return OrderedDict(phoneword)
