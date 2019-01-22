e7a1e66b001de88d74d9cca1a9cefbc8eec54f54
Albert Alises <albert.alises@gmail.com>
2019-01-22 18:21:15 +0100

    Add overflow check on Python API

--- backend-python/api/resources/phonewords.py
+++ backend-python/api/resources/phonewords.py
@@ -4,10 +4,11 @@ from flask import jsonify
 
 class PhoneWords(Resource):
     def get(self, number):
+        MAX_LENGTH = 9
         model_instance = PhoneWordModel(False, [], '')
 
-        if not number.isdigit() or ("0" in number or "1" in number):
-            model_instance = PhoneWordModel(False, [], 'Phone number not valid. Has to contain only digits and no 0/1')
+        if not number.isdigit() or ("0" in number or "1" in number) or len(number) >= MAX_LENGTH:
+            model_instance = PhoneWordModel(False, [], 'Phone number not valid. Has to contain only digits and no 0/1, maximum length 9')
             return model_instance.json(), 500
 
         phonewords = model_instance.compute_phonewords(number)
--- backend-python/tests/test_api.py
+++ backend-python/tests/test_api.py
@@ -11,32 +11,26 @@ def test_paths(client):
 def test_phoneword(client):
     data = client.get('/api/v1/phonewords/667').get_json()
 
-    assert data["success"] == True
+    assert data["success"]
     assert data["error"] == ''
     assert len(data['phonewords']) == 36
 
 def test_another_phoneword(client):
     data = client.get('/api/v1/phonewords/79').get_json()
 
-    assert data["success"] == True
+    assert data["success"]
     assert data["error"] == ''
     assert len(data['phonewords']) == 16
 
 def test_invalid_phoneword_with_letter(client):
     data = client.get('/api/v1/phonewords/notvalid').get_json()
-    assert data["success"] == False
-    assert data["error"] == 'Phone number not valid. Has to contain only digits and no 0/1'
+    assert not data["success"]
+    assert data["error"] == 'Phone number not valid. Has to contain only digits and no 0/1, maximum length 9'
 
 def test_invalid_phoneword_with_1_0(client):
     data = client.get('/api/v1/phonewords/631').get_json()
-    assert data["success"] == False
-    assert data["error"] == 'Phone number not valid. Has to contain only digits and no 0/1'
+    assert not data["success"]
+    assert data["error"] == 'Phone number not valid. Has to contain only digits and no 0/1, maximum length 9'
 
     data = client.get('/api/v1/phonewords/630').get_json()
-    assert data["success"] == False
-
-
-
-
-
-
+    assert not data["success"]
